c1215695c1c7e67f330b6d62e880218cd53561e9
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-18 22:58:50 +0530

    Laid client folder structure and added tailwindcss

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,23 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPost =  (req,res)=>{
-    res.status(200).send("This works");
+export const getPost =  async (req,res)=>{
+    try{
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json({messages: postMessages});
+    }
+    catch(error){
+        res.status(404).json({error: error.message});
+    }
 }
-export const createPost = (req, res)=>{
-    res.status(200).send("Post creation");
+export const createPost = async (req, res)=>{
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json({messages: newPost});
+    }
+    catch(error){
+        res.status(409).json({error: error.message});
+    }
 }
