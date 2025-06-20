82f62d5b6c13ae754d10374b378fe8a507928587
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-20 23:34:24 +0530

    added / route

--- server/index.js
+++ server/index.js
@@ -11,8 +11,10 @@ app.use(cors());
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use('/posts', postRoutes);
-
+app.use('/api/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to the Memories API');
+});
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL)
 .then(()=>app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)}))
