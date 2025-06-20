ec560bc87cf511eecc133d3f1cc470c9c947d76c
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-20 16:07:08 +0530

    included updatePost in Routes folder

--- server/Routes/posts.js
+++ server/Routes/posts.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import {getPost, createPost} from '../controllers/posts.js'
+import {getPost, createPost, updatePost} from '../controllers/posts.js'
 const router = express.Router();
 
 router.get('/', getPost);
 router.post('/', createPost);
-router.patch('/:id, updatePost');
+router.patch('/:id', updatePost);
 export default router;
