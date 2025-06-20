b4a37b383f4e2f2e3a7c9772541af5073fab4eba
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-20 21:25:49 +0530

    added environment variables

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ import postRoutes from './Routes/posts.js';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 app.use(cors());
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://bhumesh01:YLBq3bRCoNvajxpa@mycluster.jta6k4p.mongodb.net/";
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
 .then(()=>app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)}))
 .catch((error)=>console.log(error.message));
